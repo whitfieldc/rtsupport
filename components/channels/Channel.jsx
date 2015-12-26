9bb57feac17db44dbc518c9511f792bfb4d1bebc
Charles Whitfield <whitfield.cw@gmail.com>
2015-12-25 20:47:46 -0600

    add ChannelForm component and add commas to import statements

--- components/channels/Channel.jsx
+++ components/channels/Channel.jsx
@@ -1,4 +1,4 @@
-import React {Component} from 'react';
+import React, {Component} from 'react';
 
 class Channel extends Component{
 	onClick(e){
--- components/channels/ChannelList.jsx
+++ components/channels/ChannelList.jsx
@@ -1,4 +1,4 @@
-import React {Component} from 'react';
+import React, {Component} from 'react';
 import Channel from './Channel.jsx';
 
 class ChannelList extends Component{
